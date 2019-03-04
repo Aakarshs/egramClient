398433c46dbdb2c149ac7a5fc14d1e756938cfa8
Aakarsh <aakarshs@outlook.com>
2019-03-04 13:35:56 -0500

    Updates

---
+++ precache-manifest.beac04103982a2b22e87ee081468aef0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5aab7eaccc551fe900820e01f85dc173",
+    "url": "/egramClient/static/media/egram_logo.5aab7eac.png"
+  },
+  {
+    "revision": "00258ff07e397cfed993ab84f58abb80",
+    "url": "/egramClient/static/media/cancel.00258ff0.png"
+  },
+  {
+    "revision": "c489841a99e5f8e1b5c1",
+    "url": "/egramClient/static/js/runtime~main.c489841a.js"
+  },
+  {
+    "revision": "868650811ea2f33ef143",
+    "url": "/egramClient/static/js/main.86865081.chunk.js"
+  },
+  {
+    "revision": "99950afef7539872c3f5",
+    "url": "/egramClient/static/js/2.99950afe.chunk.js"
+  },
+  {
+    "revision": "868650811ea2f33ef143",
+    "url": "/egramClient/static/css/main.56f043ae.chunk.css"
+  },
+  {
+    "revision": "b93d3ba29238302ed2834fb868a56764",
+    "url": "/egramClient/index.html"
+  }
+];
